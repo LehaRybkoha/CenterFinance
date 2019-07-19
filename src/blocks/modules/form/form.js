b4c4a69890acf5f043442a8f86d1ba369a24e27f
Aleksei <super.rybko@mail.ru>
2019-07-19 18:50:38 +0300

    second commit

---
+++ src/blocks/modules/form/form.js
@@ -0,0 +1,17 @@
+// modules.define('form', ['i-bem-dom'], function(provide, bemDom) {
+
+// provide(bemDom.declBlock(this.name, {
+//     onSetMod: {
+//         js: {
+//             inited: function() {
+                
+//             }
+//         }
+//     }
+// }));
+
+// });
+
+function clear() {
+	document.getElementByName('search').reset();
+}
